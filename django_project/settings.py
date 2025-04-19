159515d96768a5c69cd75c30522290aa1a55706f
Xerx81 <ryuvraj832@gmail.com>
2025-04-19 17:00:07 +0530

    ch13

--- django_project/settings.py
+++ django_project/settings.py
@@ -134,6 +134,9 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "django.contrib.staticfile.storage.StaticFilesStorage"
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
